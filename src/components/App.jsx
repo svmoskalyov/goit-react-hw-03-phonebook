45cbaef2dac62d7be2efa69fb4d2e6ebd3713e2e
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-31 16:16:46 +0200

    saves contacts in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,26 @@ import Filter from './Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactsLocal = localStorage.getItem('contacts');
+    const parsedCantacts = JSON.parse(contactsLocal);
+
+    if (contactsLocal) {
+      this.setState({ contacts: parsedCantacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+    }
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
 
   addContact = ({ name, number }) => {
     const item = {
